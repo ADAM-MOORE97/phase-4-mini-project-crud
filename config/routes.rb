78587f714b2cfd6eae62eeadd7cdb8fef6ac7093
Adam <adammoore118@outlook.com>
2021-11-24 11:12:37 -0500

    pair programming

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :spices, only: [:index, :create, :update, :destroy]
+  
+
+  # get '/spices', to: 'spices#index'
+
+  # get '/spices/:id', to: 'spices#show'
+
+  # post '/spices', to: 'spices#create'
+
+  # patch '/spices/:id', to: 'spices#update'
+
+  # delete '/spices/:id', to: 'spices#destroy'
 end
